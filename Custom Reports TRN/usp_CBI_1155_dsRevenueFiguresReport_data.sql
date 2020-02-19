1d455f042ea223a17fb9462d2844b80bd327a4a1
Meidell <andre.meidell@extendaretail.com>
2020-02-19 10:53:48 +0100

    Changes to report 1155

--- Custom Reports TRN/usp_CBI_1155_dsRevenueFiguresReport_data.sql
+++ Custom Reports TRN/usp_CBI_1155_dsRevenueFiguresReport_data.sql
@@ -1,16 +1,18 @@
 USE [BI_Mart]
 GO
 
-/****** Object:  StoredProcedure [dbo].[usp_CBI_1155_dsRevenueFiguresReport_data]    Script Date: 15.01.2019 14:33:19 ******/
+/****** Object:  StoredProcedure [dbo].[usp_CBI_1155_dsRevenueFiguresReport_data]    Script Date: 19.02.2020 10:52:59 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER  PROCEDURE [dbo].[usp_CBI_1155_dsRevenueFiguresReport_data]
+
+
+CREATE PROCEDURE [dbo].[usp_CBI_1155_dsRevenueFiguresReport_data]
 (   
-   @StoreId AS VARCHAR(100),
+    @StoreId AS VARCHAR(100),
 	@PeriodType AS VARCHAR(1), 
 	@DateFrom AS DATETIME, 
 	@DateTo AS DATETIME, 
@@ -18,7 +20,7 @@ ALTER  PROCEDURE [dbo].[usp_CBI_1155_dsRevenueFiguresReport_data]
 	@RelativePeriodType AS VARCHAR(5),
 	@RelativePeriodStart AS INTEGER, 
 	@RelativePeriodDuration AS INTEGER ,	
-   @GtinFrom AS VARCHAR(50),
+    @GtinFrom AS VARCHAR(50),
 	@GtinTo AS VARCHAR(50),	
 	@ArticleIdFrom AS VARCHAR(50),
 	@ArticleIdTo AS VARCHAR(50),
@@ -26,7 +28,7 @@ ALTER  PROCEDURE [dbo].[usp_CBI_1155_dsRevenueFiguresReport_data]
 	@BrandIdTo	AS VARCHAR(50),
 	@ArticleHierarchyIdFrom AS VARCHAR(50),
 	@ArticleHierarchyIdTo AS VARCHAR(50),
-	@GroupBy AS VARCHAR(50) = 'Month', --Month, Week, WeekDay, Supplier, ArticleHierachy, Article, Brand
+	@GroupBy AS VARCHAR(50) = 'Month', --Month, Week, WeekDay, Supplier, ArticleHierarchy, Article, Brand
 	@ArticleSelectionId AS VARCHAR(1000) 
 	
 ) 
@@ -38,18 +40,25 @@ SET @GtinTo = CASE WHEN @GtinTo = '' THEN NULL ELSE @GtinTo END;
 SET @ArticleIdFrom = CASE WHEN @ArticleIdFrom = '' THEN NULL ELSE @ArticleIdFrom END;
 SET @ArticleIdTo = CASE WHEN @ArticleIdTo = '' THEN NULL ELSE @ArticleIdTo END;
 
+
+--- Converting date to dateIdx'es
+DECLARE @DateIdxBegin VARCHAR(8) = cast(convert(char(8), @DateFrom, 112) as VARCHAR(8))
+DECLARE @DateIdxEnd VARCHAR(8) = cast(convert(char(8), @DateTo, 112) as VARCHAR(8))
+
+DECLARE @QueryStringPart1 NVARCHAR(MAX)
+
 ------------------------------------------------------------------------------------------------------
 
-;WITH ArticlesInSelection AS 
-(
-SELECT cas.ArticleIdx
-FROM RBIM.Dim_ArticleSelection das
-LEFT JOIN RBIM.Cov_ArticleSelection cas ON cas.ArticleSelectionIdx = das.ArticleSelectionIdx
-WHERE das.ArticleSelectionId = @ArticleSelectionId
-)
-,SelectedSales AS (
-SELECT 
+IF (@DateFrom IS NULL) 
+BEGIN
+	SELECT TOP(0) 1 
+END
+ELSE 
+BEGIN 
 
+SET @QueryStringPart1 = '
+;WITH SelectedSales AS (
+SELECT 
  dd.[YearMonthNumber]
 ,dd.[YearWeekNumber]
 ,dsup.[SupplierName]
@@ -59,7 +68,7 @@ SELECT
 ,da.[BrandName]
 ,da.[ArticleName]
 ,da.[ArticleId]
-,ISNULL(g.Gtin, '') AS Gtin
+,g.Gtin
 ,dd.FullDate
 ,(f.[QuantityOfArticlesSold] - f.QuantityOfArticlesInReturn) AS Quantity -- Antall
 ,(f.[SalesAmount] + f.[ReturnAmount]) AS RevenueAmountInclVat -- Omsetning
@@ -70,52 +79,157 @@ join rbim.Dim_Date dd (NOLOCK) ON dd.DateIdx = f.ReceiptDateIdx
 join rbim.Dim_Article da (NOLOCK)on da.ArticleIdx = f.ArticleIdx 
 join rbim.Dim_Store ds (NOLOCK) on ds.storeidx = f.storeidx
 JOIN rbim.Dim_Supplier dsup (NOLOCK) ON dsup.SupplierIdx = f.SupplierIdx
---LEFT JOIN rbim.Cov_ArticleGtin cag ON cag.ArticleIdx = da.ArticleIdx
 LEFT JOIN RBIM.Dim_Gtin g (NOLOCK) ON g.GtinIdx = f.gtinIdx
+Where ds.StoreId =  '+ @StoreId
+ + '  and f.ReceiptDateIdx BETWEEN ' +@DateIdxBegin+' AND '+@DateIdxEnd+'
+and ds.isCurrentStore = 1  ' 	
 
-Where 
-	--ISNULL(cag.IsDefaultGtin,1) = 1
---AND ISNULL(g.isCurrent, 1) = 1
--- filter on store
-@StoreId = ds.StoreId
---  filter on period
-and (
-	(@PeriodType='D' and dd.FullDate between @DateFrom and @DateTo)
-	or (@PeriodType='Y' and dd.RelativeYTD = @YearToDate)
-	or (@PeriodType='R' and @RelativePeriodType = 'D' and dd.RelativeDay between @RelativePeriodStart and @RelativePeriodStart+@RelativePeriodDuration-1)
-	or (@PeriodType='R' and @RelativePeriodType = 'W' and dd.RelativeWeek between @RelativePeriodStart and @RelativePeriodStart+@RelativePeriodDuration-1)
-	or (@PeriodType='R' and @RelativePeriodType = 'M' and dd.RelativeMonth between @RelativePeriodStart and @RelativePeriodStart+@RelativePeriodDuration-1)
-	or (@PeriodType='R' and @RelativePeriodType = 'Q' and dd.RelativeQuarter between @RelativePeriodStart and @RelativePeriodStart+@RelativePeriodDuration-1)
-	or (@PeriodType='R' and @RelativePeriodType = 'Y' and dd.RelativeYear between @RelativePeriodStart and @RelativePeriodStart+@RelativePeriodDuration-1)
-	)
---  filter on period
---and da.ArticleIdx > -1 			-- needs to be included if you should exclude LADs etc.
-and ds.isCurrentStore = 1   	-- make sure you only get the 'current store' 
-								-- (will include all historical rows with the same PublicOrgNo and GlobalLocationNo as the current) 
-and g.Gtin
-	>= CAST(CASE WHEN @GtinFrom IS NULL THEN g.Gtin ELSE @GtinFrom END AS BIGINT)				
-and g.Gtin 
-	<= CAST(CASE WHEN @GtinTo IS NULL THEN g.Gtin ELSE @GtinTo END AS BIGINT)	
+if @GtinFrom>0
+SET @QueryStringPart1 = @QueryStringPart1+ '
+AND g.Gtin	>= CAST(CASE WHEN '+@GtinFrom+' IS NULL THEN g.Gtin ELSE '+@GtinFrom+' END AS BIGINT) '				
 
-and da.ArticleId
-	>= CAST(CASE WHEN @ArticleIdFrom IS NULL THEN da.ArticleId ELSE @ArticleIdFrom END AS INT)				
-and da.ArticleId 
-	<= CAST(CASE WHEN @ArticleIdTo IS NULL THEN da.ArticleId ELSE @ArticleIdTo END AS INT)		
+if @GtinTo>0
+SET @QueryStringPart1 = @QueryStringPart1+ '
+and g.Gtin  <= CAST(CASE WHEN '+@GtinTo+' IS NULL THEN g.Gtin ELSE '+@GtinTo+' END AS BIGINT) '
 
-and da.BrandId
-	>= CAST(CASE WHEN @BrandIdFrom IS NULL THEN da.BrandId ELSE @BrandIdFrom END AS INT)				
-and da.BrandId 
-	<= CAST(CASE WHEN @BrandIdTo IS NULL THEN da.BrandId ELSE @BrandIdTo END AS INT)	
+if @ArticleIdFrom>0
+SET @QueryStringPart1 = @QueryStringPart1+ '
+and da.ArticleId >= CAST(CASE WHEN '+@ArticleIdFrom+' IS NULL THEN da.ArticleId ELSE '+@ArticleIdFrom+' END AS INT)	'	
+		
+if @ArticleIdTo>0
+SET @QueryStringPart1 = @QueryStringPart1+ '
+and da.ArticleId <= CAST(CASE WHEN '+@ArticleIdTo+' IS NULL THEN da.ArticleId ELSE '+@ArticleIdTo+' END AS INT)	'	
+
+if @BrandIdFrom>0
+SET @QueryStringPart1 = @QueryStringPart1+ '
+and da.BrandId >= CAST(CASE WHEN '+@BrandIdFrom+' IS NULL THEN da.BrandId ELSE '+@BrandIdFrom+' END AS INT)	'		
+	
+if @BrandIdTo>0
+SET @QueryStringPart1 = @QueryStringPart1+ '
+and da.BrandId <= CAST(CASE WHEN '+@BrandIdTo+' IS NULL THEN da.BrandId ELSE '+@BrandIdTo+' END AS INT)	'
 			
-and da.Lev2ArticleHierarchyId
-	>= CAST(CASE WHEN @ArticleHierarchyIdFrom IS NULL THEN da.Lev2ArticleHierarchyId ELSE @ArticleHierarchyIdFrom END AS INT)				
-and da.Lev2ArticleHierarchyId 
-	<= CAST(CASE WHEN @ArticleHierarchyIdTo IS NULL THEN da.Lev2ArticleHierarchyId ELSE @ArticleHierarchyIdTo END AS INT)	
-AND (@ArticleSelectionId IS NULL OR da.ArticleIdx IN (SELECT * FROM ArticlesInSelection))				
-)
----------------------------------------------------------
+if @ArticleHierarchyIdFrom>0
+SET @QueryStringPart1 = @QueryStringPart1+ '
+and da.Lev2ArticleHierarchyId >= CAST(CASE WHEN '+@ArticleHierarchyIdFrom+' IS NULL THEN da.Lev2ArticleHierarchyId ELSE '+@ArticleHierarchyIdFrom+' END AS INT)	'	
+		
+if @ArticleHierarchyIdTo>0
+SET @QueryStringPart1 = @QueryStringPart1+ '
+and da.Lev2ArticleHierarchyId <= CAST(CASE WHEN '+@ArticleHierarchyIdTo+' IS NULL THEN da.Lev2ArticleHierarchyId ELSE '+@ArticleHierarchyIdTo+' END AS INT)	'
+
+if @ArticleSelectionId>0
+SET @QueryStringPart1 = @QueryStringPart1+ '
+AND da.ArticleIdx IN ('+@ArticleSelectionId+') '
+
+SET @QueryStringPart1 = @QueryStringPart1+ '			
+)'
+
+IF @ArticleSelectionId>0
+BEGIN
+SET @QueryStringPart1 = @QueryStringPart1+ '
+,ArticlesInSelection AS 
 (
+SELECT cas.ArticleIdx
+FROM RBIM.Dim_ArticleSelection das
+LEFT JOIN RBIM.Cov_ArticleSelection cas ON cas.ArticleSelectionIdx = das.ArticleSelectionIdx
+WHERE das.ArticleSelectionId = '+@ArticleSelectionId+'
+)
+'
+END
+
+IF @GroupBy = 'ArticleHierarchy'
+	BEGIN
+	SET @QueryStringPart1 = @QueryStringPart1+ '
+	SELECT
+	Lev1ArticleHierarchyName AS GroupedBy
+	,NULL AS GroupedBy2
+	,''''  AS Gtin
+	,SUM(Quantity) AS Quantity 
+	,SUM(RevenueAmountInclVat) AS RevenueAmountInclVat 
+	,SUM(Revenue) AS Revenue 
+	,SUM(PurchaseAmount) AS PurchaseAmount
+	FROM SelectedSales
+	GROUP BY Lev1ArticleHierarchyName
+	ORDER BY 1
+	'		
+	END
+
+----Month, Week, WeekDay, Supplier, ArticleHierachy, Article, Brand
+IF @GroupBy = 'Month'
+BEGIN
+SET @QueryStringPart1 = @QueryStringPart1+ '
 SELECT 
+	CONVERT(VARCHAR(10),yearMonthNumber) AS GroupedBy
+	,NULL AS GroupedBy2
+	,''''  AS Gtin
+	,SUM(Quantity) AS Quantity 
+	,SUM(RevenueAmountInclVat) AS RevenueAmountInclVat 
+	,SUM(Revenue) AS Revenue 
+	,SUM(PurchaseAmount) AS PurchaseAmount
+	FROM SelectedSales
+	GROUP BY CONVERT(VARCHAR(10),yearMonthNumber)
+	ORDER BY 1
+	'
+END
+
+
+IF @GroupBy = 'Week'
+BEGIN
+SET @QueryStringPart1 = @QueryStringPart1+ '
+SELECT 
+	 CONVERT(VARCHAR(10),yearWeekNumber) AS GroupedBy
+	,NULL AS GroupedBy2
+	,''''  AS Gtin
+	,SUM(Quantity) AS Quantity 
+	,SUM(RevenueAmountInclVat) AS RevenueAmountInclVat 
+	,SUM(Revenue) AS Revenue 
+	,SUM(PurchaseAmount) AS PurchaseAmount
+	FROM SelectedSales
+	GROUP BY CONVERT(VARCHAR(10),yearWeekNumber)
+	ORDER BY 1
+'
+END 
+
+IF @GroupBy = 'WeekDay'
+BEGIN
+SET @QueryStringPart1 = @QueryStringPart1+ '
+SELECT
+	 '''' AS GroupedBy
+	,FullDate AS GroupedBy2
+	,''''  AS Gtin
+	,SUM(Quantity) AS Quantity 
+	,SUM(RevenueAmountInclVat) AS RevenueAmountInclVat 
+	,SUM(Revenue) AS Revenue 
+	,SUM(PurchaseAmount) AS PurchaseAmount
+	FROM SelectedSales
+	GROUP BY FullDate
+	ORDER BY FullDate
+'
+END
+
+
+IF @GroupBy = 'Supplier'
+BEGIN
+SET @QueryStringPart1 = @QueryStringPart1+ '
+SELECT 
+	 SupplierName AS GroupedBy
+	,NULL AS GroupedBy2
+	,''''  AS Gtin
+	,SUM(Quantity) AS Quantity 
+	,SUM(RevenueAmountInclVat) AS RevenueAmountInclVat 
+	,SUM(Revenue) AS Revenue 
+	,SUM(PurchaseAmount) AS PurchaseAmount
+	FROM SelectedSales
+	GROUP BY  SupplierName
+	ORDER BY 1
+'
+END
+
+
+
+IF @GroupBy = 'Article'
+BEGIN
+SET @QueryStringPart1 = @QueryStringPart1+ '
+SELECT
 	ArticleName AS GroupedBy
 	,NULL AS GroupedBy2
 	,CONVERT(VARCHAR(50),Gtin) AS Gtin
@@ -124,60 +238,35 @@ SELECT
 	,SUM(Revenue) AS Revenue 
 	,SUM(PurchaseAmount) AS PurchaseAmount
 	FROM SelectedSales
-	WHERE @GroupBy = 'Article'
 	GROUP BY ArticleName, Gtin
-) 
-UNION 
-(
-SELECT 
-	NULL AS GroupedBy
-	,FullDate AS GroupedBy2
-	, '' AS Gtin
-	,SUM(Quantity) AS Quantity 
-	,SUM(RevenueAmountInclVat) AS RevenueAmountInclVat 
-	,SUM(Revenue) AS Revenue 
-	,SUM(PurchaseAmount) AS PurchaseAmount
-	FROM SelectedSales
-	WHERE @GroupBy = 'WeekDay'
-	GROUP BY FullDate
-)
-UNION
-(
-SELECT 
-	CASE @GroupBy 
-			WHEN 'Supplier' THEN SupplierName
-			WHEN 'ArticleHierarchy' THEN Lev1ArticleHierarchyName
-			WHEN 'Month' THEN CONVERT(VARCHAR(10),yearMonthNumber)
-			WHEN 'Week' THEN CONVERT(VARCHAR(10),yearWeekNumber)
-			WHEN 'Brand' THEN BrandName
-	END AS GroupedBy
-	,NULL AS GroupedBy2
-	,''  AS Gtin
-	,SUM(Quantity) AS Quantity 
-	,SUM(RevenueAmountInclVat) AS RevenueAmountInclVat 
-	,SUM(Revenue) AS Revenue 
-	,SUM(PurchaseAmount) AS PurchaseAmount
-	FROM SelectedSales
-	WHERE @GroupBy IN ('Supplier', 'ArticleHierarchy','Month','Week', 'Brand')
-	GROUP BY CASE @GroupBy 
-			WHEN 'Supplier' THEN SupplierName
-			WHEN 'ArticleHierarchy' THEN Lev1ArticleHierarchyName
-			WHEN 'Month' THEN CONVERT(VARCHAR(10),yearMonthNumber)
-			WHEN 'Week' THEN CONVERT(VARCHAR(10),yearWeekNumber)
-			WHEN 'Brand' THEN BrandName
-	END
-)
-ORDER BY GroupedBy, GroupedBy2
-
-
-
-
-
-
--------------------------------------------------------------------------------------------------------
+	ORDER BY 1,3
+'
 END
 
 
+IF @GroupBy = 'Brand'
+BEGIN
+SET @QueryStringPart1 = @QueryStringPart1+ '
+SELECT 
+	 BrandName AS GroupedBy
+	,NULL AS GroupedBy2
+	,''''  AS Gtin
+	,SUM(Quantity) AS Quantity 
+	,SUM(RevenueAmountInclVat) AS RevenueAmountInclVat 
+	,SUM(Revenue) AS Revenue 
+	,SUM(PurchaseAmount) AS PurchaseAmount
+	FROM SelectedSales
+	GROUP BY BrandName
+'
+END
+
+--PRINT @QueryStringPart1
+
+EXEC sp_executesql @QueryStringPart1
+
+END 
+
+END
 
 
 GO
